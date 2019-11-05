683a597c27a36c313b45f56cedbce4c36a54e4e8
jonesbr <brandan.jones@uc.edu>
2019-11-05 18:53:45 -0500

    Consume and output some JSON Data.

--- PlantDiary001/Pages/Index.cshtml.cs
+++ PlantDiary001/Pages/Index.cshtml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using QuickType;
 
 namespace PlantDiary001.Pages
 {
@@ -19,6 +21,15 @@ namespace PlantDiary001.Pages
 
         public void OnGet()
         {
+            
+            using (WebClient webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://www.plantplaces.com/perl/mobile/viewspecimenlocations.pl?Lat=39.14455075&Lng=-84.5093939666667&Range=0.5&Source=location&Version=2");
+                Welcome welcome = Welcome.FromJson(jsonString);
+                Specimen[] allSpecimens = welcome.Specimens;
+                ViewData["allSpecimens"] = allSpecimens;
+            }
+
             int yearStarted = 2006;
             string name = "My Plant Diary";
             ViewData["Name"] = name;
