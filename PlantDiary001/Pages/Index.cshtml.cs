f1ed1b7477ab9dfb0b76afc8a63b7e6a7cbdebae
jonesbr <brandan.jones@uc.edu>
2019-11-05 20:47:23 -0500

    Add validation to the schema.

--- PlantDiary001/Pages/Index.cshtml.cs
+++ PlantDiary001/Pages/Index.cshtml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 using QuickType;
 using QuickTypePlants;
 
@@ -20,7 +22,7 @@ namespace PlantDiary001.Pages
             _logger = logger;
         }
 
-        public JsonResult OnGet()
+        public void OnGet()
         {
 
             // this new array will hold only specimens that like water.
@@ -32,25 +34,35 @@ namespace PlantDiary001.Pages
                 Plant[] allPlants = Plant.FromJson(plantJson);
 
                 string jsonString = webClient.DownloadString("https://www.plantplaces.com/perl/mobile/viewspecimenlocations.pl?Lat=39.14455075&Lng=-84.5093939666667&Range=0.5&Source=location&Version=2");
-                Welcome welcome = Welcome.FromJson(jsonString);
-                Specimen[] allSpecimens = welcome.Specimens;
-
-                // populate a dictionary with plant data.
-                IDictionary<long, Plant> plants = new Dictionary<long, Plant>();
-                foreach(Plant plant in allPlants)
+                // do some validation before we consume the data.
+                string strSchema = System.IO.File.ReadAllText("SpecimenSchema.json");
+                JSchema schema = JSchema.Parse(strSchema);
+                JObject jsonObject = JObject.Parse(jsonString);
+                if (jsonObject.IsValid(schema))
                 {
-                    plants.Add(plant.Id, plant);
-                }
 
-                // iterate over the specimens, to find which ones like water.
-                foreach(Specimen specimen in allSpecimens)
-                {
-                    if (plants.ContainsKey(specimen.PlantId))
+                    Welcome welcome = Welcome.FromJson(jsonString);
+                    Specimen[] allSpecimens = welcome.Specimens;
+
+                    // populate a dictionary with plant data.
+                    IDictionary<long, Plant> plants = new Dictionary<long, Plant>();
+                    foreach (Plant plant in allPlants)
                     {
-                        waterLovingSpecimens.Add(specimen);
+                        plants.Add(plant.Id, plant);
                     }
-                }
 
+                    // iterate over the specimens, to find which ones like water.
+                    foreach (Specimen specimen in allSpecimens)
+                    {
+                        if (plants.ContainsKey(specimen.PlantId))
+                        {
+                            waterLovingSpecimens.Add(specimen);
+                        }
+                    }
+                } else
+                {
+                    ViewData["Message"] = "Invalid JSON";
+                }
                 // TODO only show water-loving specimens
                 ViewData["allSpecimens"] = waterLovingSpecimens;
             }
@@ -59,7 +71,7 @@ namespace PlantDiary001.Pages
             string name = "My Plant Diary";
             ViewData["Name"] = name;
 
-            return new JsonResult(waterLovingSpecimens);
+            
         }
     }
 }
