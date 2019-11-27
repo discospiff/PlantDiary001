985fc9519e04be3f24f3fb5ed14d697e92d11645
jonesbr <brandan.jones@uc.edu>
2019-11-26 21:13:30 -0500

    Start an autocomplete

---
+++ PlantDiary001/Pages/AutoCompletePlants.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlantDiary001.Pages
+{
+    public class AutoCompletePlantsModel : PageModel
+    {
+        private IList<string> plantNames = new List<string>();
+
+        public JsonResult OnGet()
+        {
+            plantNames.Add("Redbud");
+            plantNames.Add("Red Maple");
+            plantNames.Add("Red Oak");
+            plantNames.Add("Red Rose");
+            plantNames.Add("Red Lily");
+
+            return new JsonResult(plantNames);
+        }
+    }
+}
